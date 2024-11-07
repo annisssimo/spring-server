cfc5b8f35423b11efb7ccdba361be4305c232143
Anna <anyalar04@gmail.com>
2024-11-07 16:20:39 +0300

    feat: process post login request

---
+++ index.mjs
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+const HOST = 'localhost';
+const PORT = process.env.PORT || 3441;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(`Server is running on http://${HOST}:${PORT}/`);
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (
+    username === process.env.ADMIN_USERNAME &&
+    password === process.env.ADMIN_PASSWORD
+  ) {
+    return res.status(200).json({ isAuthenticated: true });
+  } else {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}/`);
+});
