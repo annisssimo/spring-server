e63ff1ea77a64fd91f0d6038b4601c2c5a4b4aad
Anna <anyalar04@gmail.com>
2024-11-12 16:04:30 +0300

    fix: httpStatusCode obj import

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,4 +1,4 @@
-import { HttpStatusCode } from '../utils/httpStatusCode';
+import { HttpStatusCode } from '../utils/httpStatusCode.js';
 
 export const errorHandler = (err, req, res) => {
   const statusCode = err.statusCode || HttpStatusCode.INTERNAL_SERVER_ERROR;
