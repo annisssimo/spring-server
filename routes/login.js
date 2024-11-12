a5022a303da206435b5a8b4a4bad3bd4881148bd
Anna <anyalar04@gmail.com>
2024-11-12 13:15:14 +0300

    fix: remove extra logic from login router

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,8 @@
 import express from 'express';
+import { login } from '../controllers/loginController.js';
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  if (
-    username === process.env.ADMIN_USERNAME &&
-    password === process.env.ADMIN_PASSWORD
-  ) {
-    return res.status(200).json({ isAuthenticated: true });
-  } else {
-    return res.status(401).json({ message: 'Invalid credentials' });
-  }
-});
+router.post('/login', login);
 
 export default router;
