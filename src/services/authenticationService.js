392b1b94e6fd580e0ccdd621cab9857e06a89c0f
Anna <anyalar04@gmail.com>
2024-12-29 00:09:11 +0300

    refactor: remove redundant validate method

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -18,26 +18,6 @@ export class AuthenticationService {
     return generateTokens(user);
   }
 
-  // static async validateSignupData(data) {
-  //   const { username, password, firstName, lastName, age } = data;
-  //   const errors = {};
-
-  //   if (!username || username.length < 3)
-  //     errors.username = 'Username must be at least 3 characters';
-  //   if (!password || !/\d/.test(password) || !/[a-zA-Z]/.test(password))
-  //     errors.password = 'Password must include at least 1 letter and 1 number';
-  //   if (password.length < 4)
-  //     errors.password = 'Password must be at least 4 characters';
-  //   if (!firstName || firstName.length < 3)
-  //     errors.firstName = 'First name must be at least 3 characters';
-  //   if (!lastName || lastName.length < 3)
-  //     errors.lastName = 'Last name must be at least 3 characters';
-  //   if (!age || isNaN(age) || age <= 0)
-  //     errors.age = 'Age must be a valid number greater than 0';
-
-  //   if (Object.keys(errors).length > 0) throw { errors };
-  // }
-
   static async hashPassword(password) {
     return bcrypt.hash(password, 10);
   }
